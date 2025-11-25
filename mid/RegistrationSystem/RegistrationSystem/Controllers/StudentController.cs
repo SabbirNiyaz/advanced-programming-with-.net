3b7cc94a485a1a41ca2fa12c516f5442168f9fd4
Sabbir Niayz <sabbirlm10niyaz@gmail.com>
2025-11-25 22:44:48 +0600

    Connect SQL Database first time

---
+++ mid/RegistrationSystem/RegistrationSystem/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using RegistrationSystem.EF;
+using RegistrationSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentsEntities db = new StudentsEntities();
+        // GET: Student
+        [HttpGet]
+        public ActionResult Resigtration()
+        {
+            return View(new RegistrationModel());
+        }
+        [HttpPost]
+        public ActionResult Resigtration(RegistrationModel model)
+        {
+            db.Students.Add(model);
+            return View(model);
+        }
+    }
+}
---
+++ mid/RegistrationSystem/RegistrationSystem/Models/RegistrationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationSystem.Models
+{
+    public class RegistrationModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
