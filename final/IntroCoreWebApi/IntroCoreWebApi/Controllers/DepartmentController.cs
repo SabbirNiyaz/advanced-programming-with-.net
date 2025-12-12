a891dacccd95e573bd78aba5305031cadaa733b6
Sabbir Niayz <sabbirlm10niyaz@gmail.com>
2025-12-12 22:05:12 +0600

    Introduction to Web API with ASP.NET Core (Basic)

---
+++ final/IntroCoreWebApi/IntroCoreWebApi/Controllers/DepartmentController.cs
@@ -0,0 +1,54 @@
+﻿using IntroCoreWebApi.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet("all")]  // Route Pattern | Url =  GET: /api/department/all
+        public IActionResult All() {
+            var d1 = new DepartmentDTO()
+            {
+                Id = 1,
+                Name = "CSE",
+            };
+            var d2 = new DepartmentDTO()
+            {
+                Id = 2,
+                Name = "EEE",
+            };
+
+            var data = new List<DepartmentDTO> { d1, d2 };
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")] // Url = GET: /api/department/{id}
+        public IActionResult Get(int id)
+        {
+            var data = new DepartmentDTO()
+            {
+                Id = id,
+                Name = $"{id}"
+            };
+            return Ok(data);
+        }
+        [HttpGet("id/{id}/name/{name}")] // Url = GET: /api/department/id/{id}/name/{name}
+        public IActionResult Get(int id, string name)
+        {
+            var data = new DepartmentDTO()
+            {
+                Id = id,
+                Name = name
+            };
+            return Ok(data);
+        }
+
+        [HttpPost("create")] // POST: /api/department/create  
+        public IActionResult Create(DepartmentDTO department)
+        {
+            return Ok(department); // Create (receive) a department object
+        }
+}
---
+++ final/IntroCoreWebApi/IntroCoreWebApi/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿// These lines import the required namespaces
+using IntroCoreWebApi.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]  // Means the route will be: api/Student (Controller)
+    [ApiController]  // Automatically handles validation and API-specific features.
+    public class StudentController : ControllerBase // ControllerBase used because this controller returns JSON (no views).
+    {
+        [HttpGet]  // GET /api/Student (Controller)
+        public IActionResult GetAll()
+        {
+            // Two StudentDTO objects are created manually
+            var s1 = new StudentDTO()
+            {
+                Id = 1,
+                Name = "Sabbir",
+                Email = "sabbir@gmail.com",
+                Phone = "01711111111"
+            };
+            var s2 = new StudentDTO()
+            {
+                Id = 2,
+                Name = "Niyaz",
+                Email = "niyaz@gmail.com",
+                Phone = "01311111111"
+            };
+
+            var data = new List<StudentDTO>() { s1, s2};  // Then they’re added to a list
+            return Ok(data); // Ok() returns an HTTP 200 status code & Sends the list of students as JSON response
+        }
+        /*
+         *** Final Output Example (JSON data):
+          [
+            {
+                "id": 1,
+                "name": "Sabbir",
+                "email": "sabbir@gmail.com",
+                "phone": "01711111111"
+            },
+            {
+                "id": 2,
+                "name": "Niyaz",
+                "email": "niyaz@gmail.com",
+                "phone": "01311111111"
+              }
+          ]
+        */
+
+
+        [HttpPost] // POST /api/Student
+        public IActionResult Post(StudentDTO student)
+        {
+            return Ok(student); // Can create object from API Testing or client side 
+        } 
+    } 
+}
+}
