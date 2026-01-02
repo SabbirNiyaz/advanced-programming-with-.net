27eb92961a577cf441dc5b9e9a23cc13e65cbf86
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2026-01-03 00:31:21 +0600

    3 Tier Arch Practice

---
+++ final/SelfPractice/ThreeTireArchitecture/PresentationTier/Controllers/CourseController.cs
@@ -0,0 +1,56 @@
+﻿using LogicTier.DTOs;
+using LogicTier.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationTier.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        CourseService service;
+        public CourseController(CourseService service)
+        {
+            this.service = service;
+        }
+
+        // Get All
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            var data = service.GetAll();
+            return Ok(data);
+
+        }
+        // Get by ID
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = service.GetById(id);
+            return Ok(data);
+        }
+        // Create
+        [HttpPost("create")]
+        public IActionResult Create(CourseDTO c)
+        {
+            var res = service.Create(c);
+            return Ok(res);
+        }
+        // Update   
+        [HttpPut("update/{id}")]
+        public IActionResult Update(int id, [FromBody] CourseDTO c)
+        {
+            c.Id = id; // make sure ID comes from URL
+            var res = service.Update(c);
+            return Ok(res);
+        }
+        // Delete
+        [HttpDelete("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var res = service.Delete(id);
+            return Ok(res);
+        }
+    }
+}
--- final/SelfPractice/ThreeTireArchitecture/PresentationTier/Program.cs
+++ final/SelfPractice/ThreeTireArchitecture/PresentationTier/Program.cs
@@ -1,3 +1,8 @@
+using DataTier.EF;
+using DataTier.Repos;
+using LogicTier.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Services and Repos
+
+builder.Services.AddScoped<CourseService>();
+builder.Services.AddScoped<CourseRepo>();
+
+
+// DI
+builder.Services.AddDbContext<UMSContext>(opt => {
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConn"));
+});
 
 var app = builder.Build();
 
