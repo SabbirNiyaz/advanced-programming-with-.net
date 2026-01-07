144f655252977760d001bc0dbe2ebd0c01c5f120
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2026-01-07 23:24:00 +0600

    1st psuh

---
+++ final/ThreeTierProjectIntro/PresentationTier/Program.cs
@@ -0,0 +1,38 @@
+using DataTier.EF;
+using DataTier.Repos;
+using LogicTier.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Own create services DI
+builder.Services.AddScoped<CategoryRepo>();
+builder.Services.AddScoped<CategoryService>();
+
+// DI
+builder.Services.AddDbContext<DatabaseRepresentContext>(opt => {
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConn"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
